b7b86520030821271d348601aa922f8fd6f11601
Shamanth Kuthpadi <shamanth.kuthpadi2003@gmail.com>
2022-08-09 19:12:16 -0400

    Completed most of UI.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let ls = window.localStorage;
+
 // DOM Extractions
 let time = document.getElementById("current-time");
 let date = document.getElementById("current-date");
+let modal = document.getElementById("exampleModalToggle");
+let modalButton = document.getElementById("modal-opener");
+let refresh = document.getElementById("refresh");
 
-
+let floor = document.getElementById("floor-input");
+let description = document.getElementById("description");
 
 // Setting the current date and time
 setInterval(() => {
@@ -12,3 +18,20 @@ setInterval(() => {
 let d = new Date();
 date.innerHTML = d.toLocaleDateString();
 
+window.onload=function(){
+    modalButton.click();
+};
+
+modalButton.style.visibility = 'hidden';
+
+refresh.addEventListener("click", () => {
+    window.location.reload();
+});
+
+floor.addEventListener("keyup", () => {
+    ls["floor"] = JSON.stringify(floor.value);
+});
+
+description.addEventListener("keyup", () => {
+    ls["description"] = JSON.stringify(description.value);
+});
